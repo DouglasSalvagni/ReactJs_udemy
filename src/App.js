e140ff270f170b95b0ef5a085c3a796cbd82fc18
unknown <douglassalvagni@gmail.com>
2019-10-22 12:25:47 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Membro from './components/membro'
+
+class App extends Component{
+
+
+    render() {
+        return(
+            <div>
+               <Membro nome="visitante"/>
+            </div>
+        );
+    }
+}
+
+export default App;
