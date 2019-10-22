ea150802c019a9ca9ec0a2a7b4eb87218afff308
Douglas <douglassalvagni@gmail.com>
2019-10-22 18:09:41 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import Membro from './components/membro'
 
 class App extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'oi',
+        };
+    }
+
+    botao() {
+        const bodyColor = document.querySelector('body')
+            .style.backgroundColor;
+        var state = this.state;
+        console.log(state.color);
+
+        if (bodyColor) {
+            document.querySelector('body')
+                .style.backgroundColor = '#9999';
+        } else {
+            document.querySelector('body')
+                .style.backgroundColor = 'red';
+        }
+    }
 
     render() {
-        return(
-            <div>
-               <Membro nome="visitante"/>
-            </div>
+    return(
+            <React.Fragment>
+                <h1 onClick={this.botao}>asd</h1>
+                <h2>sda</h2>
+            </React.Fragment>
         );
     }
 }
